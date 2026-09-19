d9d29fb557d7d81af2425e3bc4ccfd5bb1a1f47c
saloncey <miedzyniebem@gmail.com>
2026-09-19 22:23:58 +0200

    jakos smiga

--- src/features/home/home.ts
+++ src/features/home/home.ts
@@ -1,19 +1,17 @@
 import { Component } from '@angular/core';
-import { BannerImage } from '../banner-image/banner-image';
 import { Contact } from '../contact/contact';
-import { Header } from '../header/header';
 import { NotarialServices } from '../notarial-services/notarial-services';
 import { Fees } from '../fees/fees';
 import { CustomerArea } from '../customer-area/customer-area';
 import { TopNavbar } from '../../shared/top-navbar/top-navbar';
 import { LatinQuote } from '../latin-quote/latin-quote';
 import { AboutLawyer } from '../about-lawyer/about-lawyer';
-import { ForDisabled } from '../for-disabled/for-disabled';
 import { GoogleMap } from '../google-map/google-map';
 import { GoogleOpinions } from '../google-opinions/google-opinions';
+import { ForDisabled } from '../for-disabled/for-disabled';
 
 @Component({
-  imports: [Contact,  NotarialServices, Fees, CustomerArea, TopNavbar, LatinQuote, LatinQuote, AboutLawyer, ForDisabled, GoogleMap, GoogleOpinions],
+  imports: [Contact, ForDisabled, NotarialServices, Fees, CustomerArea, TopNavbar, LatinQuote, LatinQuote, AboutLawyer, GoogleMap, GoogleOpinions],
   selector: 'app-home',
   styleUrl: './home.css',
   templateUrl: './home.html',
