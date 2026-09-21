cb0b689bc0e12da972d0d69909902f2eb8226a53
saloncey <miedzyniebem@gmail.com>
2026-09-21 09:07:11 +0200

    nice changes

--- src/shared/main-layout/main-layout.ts
+++ src/shared/main-layout/main-layout.ts
@@ -3,9 +3,10 @@ import { RouterOutlet } from '@angular/router';
 import { Header } from '../../features/header/header';
 import { TopBar } from '../../features/top-bar/top-bar';
 import { BottomBar } from '../../features/bottom-bar/bottom-bar';
+import { TopNavbar } from '../top-navbar/top-navbar';
 
 @Component({
-  imports: [Header, TopBar, RouterOutlet, BottomBar],
+  imports: [Header, TopBar, RouterOutlet, BottomBar, TopNavbar],
   selector: 'app-main-layout',
   styleUrl: './main-layout.css',
   templateUrl: './main-layout.html',
