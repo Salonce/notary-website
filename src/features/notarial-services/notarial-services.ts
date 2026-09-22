0e5ed16345baa933cd0b57460244545ca53c154c
saloncey <miedzyniebem@gmail.com>
2026-09-22 16:48:31 +0200

    idk

--- src/features/notarial-services/notarial-services.ts
+++ src/features/notarial-services/notarial-services.ts
@@ -1,15 +1,18 @@
 import { Component } from '@angular/core';
 
 @Component({
-  imports: [],
-  selector: 'app-notarial-services',
-  styleUrl: './notarial-services.css',
-  templateUrl: './notarial-services.html',
+imports: [],
+selector: 'app-notarial-services',
+styleUrl: './notarial-services.css',
+templateUrl: './notarial-services.html',
 })
 export class NotarialServices {
   nieruchomosciOpen = false;
   rodzinneOpen = false;
   spadkiOpen = false;
+  spolkiOpen = false;
+  poswiadczeniaOpen = false;
+  pozostaleOpen = false;
 
   toggleNieruchomosci(): void {
     this.nieruchomosciOpen = !this.nieruchomosciOpen;
@@ -21,5 +24,17 @@ export class NotarialServices {
 
   toggleSpadki(): void {
     this.spadkiOpen = !this.spadkiOpen;
+  }
+
+  toggleSpolki(): void {
+    this.spolkiOpen = !this.spolkiOpen;
+  }
+
+  togglePoswiadczenia(): void {
+    this.poswiadczeniaOpen = !this.poswiadczeniaOpen;
+  }
+
+  togglePozostale(): void {
+    this.pozostaleOpen = !this.pozostaleOpen;
   }
 }
